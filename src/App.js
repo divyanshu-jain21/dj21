28e3b0f483a964c3dad8883ddca742f280a634fa
divyanshu-jain21 <divyanshujain1794@gmail.com>
2024-01-07 08:20:45 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <SocialLink/>
       <Routes>
       <Route path="/" element={<Home1/>} />
-      <Route path="/home/VIT:Vinitika-Infotech" element={<Home1/>} />
+      <Route path="/home/VIT:Vinitika-Infotech" active element={<Home1/>} />
       <Route path="/portfolio/VIT:Vinitika-Infotech" element={<Portfolio/>} />
       <Route path="/about-us/VIT:Vinitika-Infotech" element={<Aboutus/>} />
       <Route path="/contact/VIT:Vinitika-Infotech" element={<Contact/>} />
